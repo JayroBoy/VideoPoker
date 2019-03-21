ce2487cdb13681bc0590c735f598d0f2f4aeefc3
Laura Genari <44553125+LauraGenari@users.noreply.github.com>
2019-03-21 12:43:59 -0300

    Add files via upload

--- Creditos.java
+++ Creditos.java
@@ -1,6 +1,18 @@
 
-
-
 public class Creditos {
 
+	private int taoks;
+	
+	public Creditos(int k) {
+		this.setCreditos(k);
+	}
+	
+	public int getCreditos(){
+		return taoks;
+	}
+	
+	public void setCreditos(int j) {
+		taoks = j;
+	}
+	
 }
---
+++ poker.java
@@ -0,0 +1,25 @@
+public class poker {
+
+	public static void main (String args[]) throws Exception {
+		
+	String enter;
+		
+	System.out.println("Bem vindo ao Video poker. Digite ENTER para iniciar a rodada");
+	
+	enter = EntradaTeclado.leString();
+	
+	if (enter == "\n") {
+		
+		//printa os creditos da pessoa
+		//printa a mesa
+		//printa as cartas dela
+		
+		//da a opção de apostar mais ou ficar ok
+		
+		//se apostar mais
+		
+	}
+	
+	
+	}
+}
