b376cb4add62719b6f28e0b42154529e573cf3e0
Laura Genari <44553125+LauraGenari@users.noreply.github.com>
2019-03-21 21:17:10 -0300

    Add files via upload

--- Aposta.java
+++ Aposta.java
@@ -5,15 +5,18 @@ public class Aposta {
 	private int[] freqnumero;
 	private int[] freqnaipe;
 	private int aposta;
-	
+	/*
+	 * Método Construtor que inicia todas as variaveis da classe com 0, e a aposta
+	 * com o que foi apostado
+	 * */
 	public Aposta(int bet){
-		aposta = bet;
-		mult = 0;
-		par = 0;
-		trinca = 0;
-		quadra = 0;
-		freqnumero = new int[13];
-		freqnaipe = new int[4];
+		aposta = bet;	//guarda a quantia q foi apostada
+		mult = 0;	//guarda o multiplicador da aposta 
+		par = 0;	//indica pares
+		trinca = 0;	//indica trinca
+		quadra = 0;	//indica quadra
+		freqnumero = new int[13];	//vetor para contar frequencia de cada número
+		freqnaipe = new int[4]; 	//vetor para contar frequencia de cada naipe
 		for(int i = 0; i < 13; i++) {
 			freqnumero[i] = 0;
 		}
@@ -22,95 +25,106 @@ public class Aposta {
 		}
 		
 	}
-	
+	/**
+	 * @param aposta a ser atribuida 
+	 */
 	public void setAposta(int k) {
 		aposta = k;
 	}
-	
+	/**
+	 * @return a aposta
+	 */
 	public int getAposta() {
 		return aposta;
 	}
-	
+	/*Método que calcula a pontuação dado a sequencia de cartas que o 
+	 * jogador tem
+	 * @param Carta[] card tor com as 5 cartas que o jogador tem
+	 * @return O valor da aposta
+	 * */
 	public int calcular( Carta[] card) {
-	
+		//percorre o vetor de cartas para indicar quais cartas estao na mao do jogador
 		for(int i = 0; i < 5; i++) {
 			naipe = card[i].getNaipe();
 			numero = card[i].getNumero();
-			
+			/*o vetor de frequencias do naipe é tem cada posição incrementada conforme aparece determinado naipe no vetor de cartas*/
 			switch(naipe) {
-				case 0:
+				case 0
 					freqnaipe[0] +=1;
 				break;
-				case 1:
+				case 1
 					freqnaipe[1] +=1;
 					break;
-				case 2:
+				case 2
 					freqnaipe[2] +=1;
 					break;
-				case 3:
+				case 3
 					freqnaipe[3] +=1;
 					break;
-				default:
+				default
 					break;
 				}
-					
+				/*o vetor de frequencias dos numeros  tem cada posição incrementada conforme aparecem no vetor de cartas*/		
 				switch(numero) {
-				case 0:
+				case 0
 					freqnumero[0] +=1;
 				break;
-				case 1:
+				case 1
 					freqnumero[1] +=1;
 					break;
-				case 2:
+				case 2
 					freqnumero[2] +=1;
 					break;
-				case 3:
+				case 3
 					freqnumero[3] +=1;
 					break;
-				case 4:
+				case 4
 					freqnumero[4] +=1;
-				break;
-				case 5:
+					break;
+				case 5
 					freqnumero[5] +=1;
 					break;
-				case 6:
+				case 6
 					freqnumero[6] +=1;
 					break;
-				case 7:
+				case 7
 					freqnumero[7] +=1;
 					break;
-				case 8:
+				case 8
 					freqnumero[8] +=1;
-				break;
-				case 9:
+					break;
+				case 9
 					freqnumero[9] +=1;
 					break;
-				case 10:
+				case 10
 					freqnumero[10] +=1;
 					break;
-				case 11:
+				case 11
 					freqnumero[11] +=1;
 					break;
-				case 12:
+				case 12
 					freqnumero[12] +=1;
-				default:
+				default
 					break;
 				}
-			}			
-		numero = 0;
-		naipe = 0;
+			}
+		//zera as variaveis para reutilizá-las
+		numero = 0; //indica sequencia
+		naipe = 0;	//indica naipe
 		
+		//percorre o vetor de numeros para procurar as combinações que premiam o jogador
 		for(int i = 0; i < 13; i++) {
 			if(freqnumero[i] == 2) par++;
 			if(freqnumero[i] == 3) trinca++;
 			else if(freqnumero[i] == 4) quadra++;
 			else if(freqnumero[i] == 1 && (freqnumero[i-1] == 1 || i-1<0)) numero++;
 		}
-		
+		//percorre o vetor de naipes para procurar as combinações que premiam o jogador
 		for(int i = 0; i <4; i++) {
 			if(freqnaipe[i] == 1 && (freqnaipe[i-1] == 1 || i-1<0)) naipe ++;
 		}
 		
+		//atribui a pontuação de acordo com as combinações 
 		if (par == 2) mult = 1;	//dois pares
 		if (naipe == 5) mult = 10; //flush
 		else if (trinca == 1) { 
@@ -129,5 +143,3 @@ public class Aposta {
 		return mult * aposta;
 		
 	}
-}
-
--- Jogador.java
+++ Jogador.java
@@ -3,46 +3,61 @@ public class Jogador {
 	Creditos money;
 	Aposta bet;
 	Carta[] mao;
-	
+	/*Método construtor que cria um vetor de cartas, placar e créditos */
 	public Jogador() {
 		mao = new Carta[5];
 		bet = new Aposta(0);
 		money = new Creditos(200);
 		this.generateMao();
 	}
-	
+	/*inicializa cada carta do jogador*/
 	public void generateMao() {
 		for(int i = 0; i < 5; i++) {
 			mao[i] = new Carta();
 		}
 	}
-	
+	/**
+	 * @return Carta[] - retorna cada carta que tem na mao do usuario	 *
+	 */
 	public Carta[] getMao() {
 		return this.mao;
 	}
-	
+	/**
+	 * @param Carta[] dealer - recebe as cartas que estao na mesa e as entrega ao jogador
+	 */
 	public void setMao(Carta[] dealer) {
 		mao = dealer;
 	}
-	
+	/**
+	 * @return - retorna a quantidade de creditor atual do jogador 
+	 * */
 	public int getCreditos() {
 		return money.getCreditos();
 	}
-	
+	/**
+	 *@param k - atribui o valor k aos créditos
+	 */
 	public void setCreditos(int k) {
 		money.setCreditos(k);
 	}
-	
+	/**
+	 *@param k - atribui o valor k a aposta
+	 */
 	public void setAposta(int k) {
 		bet.setAposta(k);
 	}
-	
+	/**
+	 *@return k - return o valor da aposta
+	 */
 	public int getAposta() {
 		return bet.getAposta();
 	}
 	
 	
-	//Altera o numero de creditos para o que tenho na carteira + a aposta * o multiplicador
+	/**
+	*Altera o numero de creditos para o que tenho na carteira + a aposta * o multiplicador
+	*@return o tanto de creditos que o jogador ganhou
+	*/
 	public void cashIn() {
 		this.money.setCreditos(this.bet.calcular(mao) + money.getCreditos());
 	}
